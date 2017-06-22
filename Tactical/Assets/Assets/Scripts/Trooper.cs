12915b61e5b9bb68f3f435830d0f0f503f72c64b
aaronhillier123 <aaron123@tamu.edu>
2017-06-21 21:39:49 -0500

    Starting Unity Project
    Unity game with model/map/animation added. Also arrow pad movement.

---
+++ Tactical/Assets/Assets/Scripts/Trooper.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trooper : MonoBehaviour {
+
+	private Animator animator;
+	private int animInt;
+	// Use this for initialization
+	void Start () {
+
+		animator = gameObject.GetComponentInChildren<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("left")) {
+			rotateTo (180.0f);
+			move ();
+		} else if (Input.GetKey ("down")) {
+			rotateTo (90f);
+			move ();
+		} else if (Input.GetKey ("up")) {
+			rotateTo (270f);
+			move ();
+		} else if(Input.GetKey ("right")) {
+			rotateTo (0f);
+			move ();
+		} else {
+			stop();
+		}
+	}
+
+	void rotateTo (float a){
+		float rotationPerMinute;
+		Quaternion myRotation = gameObject.transform.rotation;
+		float moveSpeed = .01f;
+		gameObject.transform.rotation = Quaternion.Lerp(myRotation, Quaternion.Euler(0, a, 0), moveSpeed * Time.time);
+	}
+
+	void stop(){
+		animator.SetInteger ("AnimPar", 0);
+	}
+
+	void move(){
+		animator.SetInteger ("AnimPar", 1);
+		gameObject.transform.Translate ( 0f, 0f, 0.25f);
+	}
+}
